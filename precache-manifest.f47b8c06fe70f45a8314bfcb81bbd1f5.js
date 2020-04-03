9df46aca636c965af9a8f8d53957681339bb7ce8
Bayram <bayram.hendem@qmarketing.de>
2020-04-03 19:03:03 +0200

    Updates

---
+++ precache-manifest.f47b8c06fe70f45a8314bfcb81bbd1f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "291bdaee9a47293bf01c8fedf804ed12",
+    "url": "./index.html"
+  },
+  {
+    "revision": "73c6fac1c91b640a7b8e",
+    "url": "./static/css/main.a592396b.chunk.css"
+  },
+  {
+    "revision": "fcfafd2d37e4cc66550e",
+    "url": "./static/js/2.1d1f1668.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "./static/js/2.1d1f1668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73c6fac1c91b640a7b8e",
+    "url": "./static/js/main.caa59b20.chunk.js"
+  },
+  {
+    "revision": "f68efe28d5435a5c1a8d",
+    "url": "./static/js/runtime-main.7e0a6fea.js"
+  },
+  {
+    "revision": "f132e4ffd7ac6324716962c1459d459b",
+    "url": "./static/media/SFProDisplay-BlackItalic.f132e4ff.ttf"
+  }
+]);
